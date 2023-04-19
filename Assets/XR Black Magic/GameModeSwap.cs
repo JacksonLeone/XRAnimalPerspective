d614b63b9df2c7fe3f133825fdba42f7c180a020
asantmier <allen.santmier@gmail.com>
2023-04-19 13:57:22 -0400

    Fix Teleport

--- Assets/XR Black Magic/GameModeSwap.cs
+++ Assets/XR Black Magic/GameModeSwap.cs
@@ -26,14 +26,14 @@ public class GameModeSwap : MonoBehaviour
     {
         if (inVR)
         {
-            desktopPlayer.transform.position = vrPlayer.transform.position;
+            desktopPlayer.transform.position = vrPlayer.transform.position + new Vector3(0, 0.5f, 0);
             desktopPlayer.transform.rotation = vrPlayer.transform.rotation;
             vrPlayer.SetActive(false);
             desktopPlayer.SetActive(true);
             inVR = false;
         } else
         {
-            vrPlayer.transform.position = desktopPlayer.transform.position;
+            vrPlayer.transform.position = desktopPlayer.transform.position + new Vector3(0, 0.5f, 0);
             vrPlayer.transform.rotation = desktopPlayer.transform.rotation;
             vrPlayer.SetActive(true);
             desktopPlayer.SetActive(false);
